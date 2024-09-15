797a9d26542a5bd6e507fe689ae253dbddda3353
Fasalu Rahman <145885597+fa-salu@users.noreply.github.com>
2024-09-15 12:20:52 +0530

    Delete babyshop-E-Commerce-Server-main directory

--- babyshop-E-Commerce-Server-main/models/wishlistModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const wishlistSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", unique: true },
-  products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
-});
-
-module.exports = mongoose.model("Wishlist", wishlistSchema);
